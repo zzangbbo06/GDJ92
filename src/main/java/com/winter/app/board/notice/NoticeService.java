4432064fd2d077db480f1651316b636b86e9e32c
zzangbbo06 <zzangbbo06>
2025-08-12 14:39:55 +0900

    up

--- src/main/java/com/winter/app/board/notice/NoticeService.java
+++ src/main/java/com/winter/app/board/notice/NoticeService.java
@@ -8,6 +8,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.winter.app.board.BoardFileVO;
@@ -17,6 +18,7 @@ import com.winter.app.commons.FileManager;
 import com.winter.app.commons.Pager;
 
 @Service
+@Transactional
 public class NoticeService implements BoardService{
 
 	@Autowired
@@ -47,7 +49,8 @@ public class NoticeService implements BoardService{
 	 * 
 	 * return noticeDAO.add(boardVO); }
 	 */
-
+	
+	@Transactional
 	@Override
 	public int insert(BoardVO boardVO, MultipartFile [] attaches) throws Exception {
 		int result = noticeDAO.insert(boardVO);
